b44f09a7092000c700dae3527eb45e6f16a9c815
str <str_ie@yahoo.co.uk>
2017-06-18 20:30:23 +0200

    hacked to load from JSON URL + plainer map

--- index.js
+++ index.js
@@ -1,82 +1,90 @@
-( function ( $, L, prettySize ) {
+// L = leaflet
+(function ($, L, prettySize) {
 	var map, heat,
 		heatOptions = {
 			tileOpacity: 1,
 			heatOpacity: 1,
 			radius: 25,
-			blur: 15
+			blur: 15,
+			//gradient: { 0: 'blue', 1: 'red' }
 		};
 
 	// Start at the beginning
 	stageOne();
 
-	function stageOne () {
+	function stageOne() {
 		var dropzone;
 
 		// Initialize the map
-		map = L.map( 'map' ).setView([0,0], 2);
-		L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		map = L.map('map').setView([0, 0], 2);
+
+		// L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		L.tileLayer('http://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 			attribution: 'location-history-visualizer is open source and available <a href="https://github.com/theopolisme/location-history-visualizer">on GitHub</a>. Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors.',
 			maxZoom: 18,
 			minZoom: 2
-		} ).addTo( map );
+		}).addTo(map);
 
 		// Initialize the dropzone
-		dropzone = new Dropzone( document.body, {
+		dropzone = new Dropzone(document.body, {
 			url: '/',
-			previewsContainer: document.createElement( 'div' ), // >> /dev/null
+			previewsContainer: document.createElement('div'), // >> /dev/null
 			clickable: false,
-			accept: function ( file, done ) {
-				stageTwo( file );
+			accept: function (file, done) {
+				stageTwo(file);
 				dropzone.disable(); // Your job is done, buddy
 			}
-		} );
+		});
 
 		// For mobile browsers, allow direct file selection as well
-		$( '#file' ).change( function () {
-			stageTwo( this.files[0] );
+		$('#file').change(function () {
+			stageTwo(this.files[0]);
 			dropzone.disable();
-		} );
+		});
+
+		stageTwo(null);
 	}
 
-	function stageTwo ( file ) {
-		heat = L.heatLayer( [], heatOptions ).addTo( map );
+	function stageTwo(file) {
+		heat = L.heatLayer([], heatOptions).addTo(map);
 
 		// First, change tabs
-		$( 'body' ).addClass( 'working' );
-		$( '#intro' ).addClass( 'hidden' );
-		$( '#working' ).removeClass( 'hidden' );
+		$('body').addClass('working');
+		$('#intro').addClass('hidden');
+		$('#working').removeClass('hidden');
 
 		// Now start working!
-		processFile( file );
+		processFile(file);
 
-		function status ( message ) {
-			$( '#currentStatus' ).text( message );
+		function status(message) {
+			$('#currentStatus').text(message);
 		}
 
-		function processFile ( file ) {
-			var fileSize = prettySize( file.size ),
+		function processFile(file) {
+			let fileSize = file ? file.size : 0;
+
+			fileSize = prettySize(fileSize),
 				reader = new FileReader();
 
-			status( 'Preparing to import file (' + fileSize + ')...' );
+			status('Preparing to import file (' + fileSize + ')...');
 
-			function getLocationDataFromJson ( data ) {
+			function getLocationDataFromJson(data) {
 				var SCALAR_E7 = 0.0000001, // Since Google Takeout stores latlngs as integers
-					locations = JSON.parse( data ).locations;
+					locations = JSON.parse(data).locations;
 
-				if ( !locations || locations.length === 0 ) {
-					throw new ReferenceError( 'No location data found.' );
+				if (!locations || locations.length === 0) {
+					throw new ReferenceError('No location data found.');
 				}
 
-				return locations.map( function ( location ) {
-					return [ location.latitudeE7 * SCALAR_E7, location.longitudeE7 * SCALAR_E7 ];
-				} );
+				return locations.map(function (location) {
+					return [location.latitudeE7 * SCALAR_E7, location.longitudeE7 * SCALAR_E7];
+				});
 			}
 
-			function getLocationDataFromKml ( data ) {
+			function getLocationDataFromKml(data) {
 				var KML_DATA_REGEXP = /<when>(.*?)<\/when>\s*<gx:coord>(\S*)\s(\S*)\s(\S*)<\/gx:coord>/g,
 					locations = [],
-					match = KML_DATA_REGEXP.exec( data );
+					match = KML_DATA_REGEXP.exec(data);
 
 				// match
 				//  [1] ISO 8601 timestamp
@@ -84,108 +92,134 @@
 				//  [3] latitude
 				//  [4] altitude (not currently provided by Location History)
 
-				while ( match !== null ) {
-					locations.push( [ Number( match[3] ), Number( match[2] ) ] );
-					match = KML_DATA_REGEXP.exec( data );
+				while (match !== null) {
+					locations.push([Number(match[3]), Number(match[2])]);
+					match = KML_DATA_REGEXP.exec(data);
 				}
 
 				return locations;
 			}
 
-			reader.onprogress = function ( e ) {
-				var percentLoaded = Math.round( ( e.loaded / e.total ) * 100 );
-				status( percentLoaded + '% of ' + fileSize + ' loaded...' );
+			reader.onprogress = function (e) {
+				var percentLoaded = Math.round((e.loaded / e.total) * 100);
+				status(percentLoaded + '% of ' + fileSize + ' loaded...');
 			};
 
-			reader.onload = function ( e ) {
+			let onLoad = function (e) {
 				var latlngs;
 
-				status( 'Generating map...' );
+				status('Generating map...');
 
 				try {
-					if ( /\.kml$/i.test( file.name ) ) {
-						latlngs = getLocationDataFromKml( e.target.result );
+					if (/\.kml$/i.test(file.name)) {
+						latlngs = getLocationDataFromKml(e.target.result);
 					} else {
-						latlngs = getLocationDataFromJson( e.target.result );
+						latlngs = getLocationDataFromJson(e.target.result);
 					}
-				} catch ( ex ) {
-					status( 'Something went wrong generating your map. Ensure you\'re uploading a Google Takeout JSON file that contains location data and try again, or create an issue on GitHub if the problem persists. (error: ' + ex.message + ')' );
+				} catch (ex) {
+					status('Something went wrong generating your map. Ensure you\'re uploading a Google Takeout JSON file that contains location data and try again, or create an issue on GitHub if the problem persists. (error: ' + ex.message + ')');
 					return;
 				}
 
 				heat._latlngs = latlngs;
 
 				heat.redraw();
-				stageThree( /* numberProcessed */ latlngs.length );
+				stageThree( /* numberProcessed */ latlngs.length);
 			};
+
+			reader.onload = onLoad;
 
 			reader.onerror = function () {
-				status( 'Something went wrong reading your JSON file. Ensure you\'re uploading a "direct-from-Google" JSON file and try again, or create an issue on GitHub if the problem persists. (error: ' + reader.error + ')' );
+				status('Something went wrong reading your JSON file. Ensure you\'re uploading a "direct-from-Google" JSON file and try again, or create an issue on GitHub if the problem persists. (error: ' + reader.error + ')');
 			};
 
-			reader.readAsText( file );
+			if(file) {
+				reader.readAsText(file);
+			} else {
+				//use some test data:
+				let smallData = '/data/SR%20-%20Location%20History/extract%20-%20large.txt';
+
+				//private data - is NOT in git!
+				let bigPrivateData = '/data/SR%20-%20Location%20History/private/reduced.json';
+
+				let activeUrl = bigPrivateData;
+				$.get(activeUrl,
+				null,
+				function(data) {
+					file = {
+						name: 'testData1.json'
+					};
+					onLoad( {
+						target: {
+							result: data
+						}
+					});
+				});
+			}
 		}
 	}
 
-	function stageThree ( numberProcessed ) {
-		var $done = $( '#done' );
-
+	function stageThree(numberProcessed) {
 		// Change tabs :D
-		$( 'body' ).removeClass( 'working' );
-		$( '#working' ).addClass( 'hidden' );
-		$done.removeClass( 'hidden' );
+		$('body').removeClass('working');
+		$('#working').addClass('hidden');
 
 		// Update count
-		$( '#numberProcessed' ).text( numberProcessed.toLocaleString() );
+		$('#numberProcessed').text(numberProcessed.toLocaleString());
 
 		// Fade away when clicked
-		$done.one( 'click', function () {
-			$( 'body' ).addClass( 'map-active' );
-			$done.fadeOut();
+		let hideDone = function () {
+			$('body').addClass('map-active');
 			activateControls();
-		} );
+		};
+		hideDone();
 
-		function activateControls () {
-			var $tileLayer = $( '.leaflet-tile-pane' ),
-				$heatmapLayer = $( '.leaflet-heatmap-layer' ),
-				originalHeatOptions = $.extend( {}, heatOptions ); // for reset
+		function activateControls() {
+			var $tileLayer = $('.leaflet-tile-pane'),
+				$heatmapLayer = $('.leaflet-heatmap-layer'),
+				originalHeatOptions = $.extend({}, heatOptions); // for reset
 
 			// Update values of the dom elements
-			function updateInputs () {
+			function updateInputs() {
 				var option;
-				for ( option in heatOptions ) {
-					if ( heatOptions.hasOwnProperty( option ) ) {
-						document.getElementById( option ).value = heatOptions[option];
+				for (option in heatOptions) {
+					if (heatOptions.hasOwnProperty(option)) {
+						let elem = document.getElementById(option);
+						if (elem) {
+							elem.value = heatOptions[option];
+						} else {
+							console.warn('could not find control for option: ', option);
+						}
 					}
 				}
 			}
 
 			updateInputs();
 
-			$( '.control' ).change( function () {
-				switch ( this.id ) {
+			$('.control').change(function () {
+				switch (this.id) {
 					case 'tileOpacity':
-						$tileLayer.css( 'opacity', this.value );
+						$tileLayer.css('opacity', this.value);
 						break;
 					case 'heatOpacity':
-						$heatmapLayer.css( 'opacity', this.value );
+						$heatmapLayer.css('opacity', this.value);
 						break;
 					default:
-						heatOptions[ this.id ] = Number( this.value );
-						heat.setOptions( heatOptions );
+						heatOptions[this.id] = Number(this.value);
+						heat.setOptions(heatOptions);
 						break;
 				}
-			} );
+			});
 
-			$( '#reset' ).click( function () {
-				$.extend( heatOptions, originalHeatOptions );
+			$('#reset').click(function () {
+				$.extend(heatOptions, originalHeatOptions);
 				updateInputs();
-				heat.setOptions( heatOptions );
+				heat.setOptions(heatOptions);
 				// Reset opacity too
-				$heatmapLayer.css( 'opacity', originalHeatOptions.heatOpacity );
-				$tileLayer.css( 'opacity', originalHeatOptions.tileOpacity );
-			} );
+				$heatmapLayer.css('opacity', originalHeatOptions.heatOpacity);
+				$tileLayer.css('opacity', originalHeatOptions.tileOpacity);
+			});
 		}
 	}
 
-}( jQuery, L, prettySize ) );
+}(jQuery, L, prettySize));
